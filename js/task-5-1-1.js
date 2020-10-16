20ed91b599ddbf808ec76eb1c269f258ec33aec8
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-10-16 23:09:49 +0300

    task-5-2 решение

--- js/task-5-1-1.js
+++ js/task-5-1-1.js
@@ -1,4 +1,3 @@
-// Write code under this line
 const Account = function (login, email) {
   this.login = login;
   this.email = email;
@@ -6,15 +5,13 @@ const Account = function (login, email) {
 Account.prototype.getInfo = function () {
   return `login: ${this.login}, email: ${this.email}`;
 };
-console.log(typeof Account.prototype.getInfo);
-// 'function'
+console.log(Account.prototype.getInfo); // function
 
 const mango = new Account("Mangozedog", "mango@dog.woof");
-console.log(mango.getInfo());
-mango.getInfo();
-// 'login : Mangozedog, email: mango@dog.woof'
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
 
 const poly = new Account("Poly", "poly@mail.com");
 console.log(poly.getInfo());
 poly.getInfo();
-// 'login : Poly, email: poly@mail.com'
+// Login: Poly, Email: poly@mail.com
