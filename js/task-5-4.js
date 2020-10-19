5f20def7ba226b0ad8cf191441a274b190909439
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-10-19 22:24:49 +0300

    task5-4 решение

---
+++ js/task-5-4.js
@@ -0,0 +1,36 @@
+class StringBuilder {
+  _value = "";
+  constructor(value) {
+    this._value = value;
+  }
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+    this._value += str;
+    return this._value;
+  }
+
+  prepend(str) {
+    this._value = str + this._value;
+  }
+
+  pad(str) {
+    this.append(str);
+    this.prepend(str);
+  }
+}
+console.log(typeof StringBuilder);
+// 'function'
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
