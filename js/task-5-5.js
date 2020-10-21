2767d2fb80123fb66db84a28ed87862fa6d10bc1
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-10-21 23:26:05 +0300

    task5-5 5 проба задати початкові умови

--- js/task-5-5.js
+++ js/task-5-5.js
@@ -1,26 +1,64 @@
 class Car {
-  // Write code under this line
-  constructor(speed = 0, price, maxSpeed, isOn = false, distance = 0) {
-    this.speed = speed;
-    this.price = price;
+  //Write code under this line
+  maxSpeed = 0;
+  speed = 0;
+  isOn = false;
+  distance = 0;
+  price = 0;
+  static getSpecs() {
+    return `maxSpeed: ${this.maxSpeed}, speed: ${this.speed}, isOn: ${this.isOn}, distance: ${this.distance}, price:${this._price}`;
+  }
+  constructor(
+    maxSpeed = 500,
+    speed = 0,
+    isOn = false,
+    distance = 0,
+    price = 2000
+  ) {
     this.maxSpeed = maxSpeed;
+    this.speed = speed;
     this.isOn = isOn;
     this.distance = distance;
+    this._price = price;
   }
-  static getSpecs(car) {
-    return `maxSpeed: $(this.maxSpeed) price: $(this.price)`;
+  get price() {
+    return this._price;
   }
-  get price() {}
-  set price(value) {}
-  turnOn() {}
-  turnOff() {}
-  accelerate(value) {}
-  decelerate(value) {}
-  drive(hours) {}
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    console.log(this.speed);
+    let summ = this.speed + value;
+    if (summ <= this.maxSpeed) {
+      this.speed = summ;
+    } else {
+      this.speed = this.speed;
+    }
+  }
+  decelerate(value) {
+    let resultSpeed = this.speed - value;
+    if (resultSpeed >= 0) {
+      this.speed = resultSpeed;
+    } else {
+      this.speed = 0;
+    }
+  }
+  drive(hours) {
+    if ((this.isOn = true)) this.distance = hours * this.speed;
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
 mustang.turnOn();
+console.log(mustang.turnOn);
 mustang.accelerate(50);
 mustang.drive(2);
 
